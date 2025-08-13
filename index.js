b5492949d9886b5596ee02242b0dc8c0f25a5bd1
ravigill3936 <gill1234ravi@gmail.com>
2025-08-13 18:11:20 -0400

    serialize and deserialize binary tree

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 class TreeNode {
-  constructor(val, left = null, right = null) {
+  constructor(val = 0, left = null, right = null) {
     this.val = val;
     this.left = left;
     this.right = right;
@@ -8,28 +8,55 @@ class TreeNode {
 
 const root = new TreeNode(
   1,
-  new TreeNode(2, new TreeNode(4), new TreeNode(5)),
-  new TreeNode(3, new TreeNode(6), new TreeNode(7))
+  new TreeNode(2),
+  new TreeNode(3, new TreeNode(4), new TreeNode(5))
 );
 
-function PO(root) {
-  let res = [];
+function serialize(root) {
+  let str = "";
 
-  function bt(r) {
-    if (r == null) {
+  function dfs(r) {
+    if (r === null) {
+      str = str + "null" ;
       return;
     }
-    res.push(r.val);
+    str = str + String(r.val) + ",";
+    dfs(r.left);
 
-    bt(r.left);
-
-    bt(r.right);
+    str = str + ",";
+    dfs(r.right);
   }
 
-  bt(root);
+  dfs(root);
 
-  return res;
+  return str;
 }
 
-let res = PO(root);
+function deserialize(data) {
+  let values = data.split(",");
+
+  let index = 0;
+
+  function buildTree() {
+    if (values[index] === "null") {
+      index++;
+      return null;
+    }
+
+    let node = new TreeNode(Number(values[index]));
+    index++;
+
+    node.left = buildTree();
+    node.right = buildTree();
+
+    return node;
+  }
+
+  return buildTree();
+}
+
+let res = serialize(root);
+
+res = deserialize(res);
+
 console.log(res);
