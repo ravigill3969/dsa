c080665f4cc2575465b5d4d2654e87bed6522705
ravigill3936 <gill1234ravi@gmail.com>
2025-08-14 15:28:15 -0400

    has duplicate dsa que

--- index.js
+++ index.js
@@ -1,62 +1,18 @@
-class TreeNode {
-  constructor(val = 0, left = null, right = null) {
-    this.val = val;
-    this.left = left;
-    this.right = right;
-  }
-}
-
-const root = new TreeNode(
-  1,
-  new TreeNode(2),
-  new TreeNode(3, new TreeNode(4), new TreeNode(5))
-);
-
-function serialize(root) {
-  let str = "";
-
-  function dfs(r) {
-    if (r === null) {
-      str = str + "null" ;
-      return;
-    }
-    str = str + String(r.val) + ",";
-    dfs(r.left);
-
-    str = str + ",";
-    dfs(r.right);
-  }
-
-  dfs(root);
-
-  return str;
-}
-
-function deserialize(data) {
-  let values = data.split(",");
-
-  let index = 0;
-
-  function buildTree() {
-    if (values[index] === "null") {
-      index++;
-      return null;
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+function hasDuplicate(nums) {
+  let set = new Set();
+  for (let i = 0; i < nums.length; i++) {
+    if (set.has(nums[i])) {
+      return true;
     }
 
-    let node = new TreeNode(Number(values[index]));
-    index++;
-
-    node.left = buildTree();
-    node.right = buildTree();
-
-    return node;
+    set.add(nums[i]);
   }
 
-  return buildTree();
+  return false;
 }
 
-let res = serialize(root);
-
-res = deserialize(res);
-
-console.log(res);
+hasDuplicate((nums = [1, 2, 3, 3]));
