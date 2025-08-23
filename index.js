1f1ce0d3c5174638f93363822aeedd833350ba40
ravigill3936 <gill1234ravi@gmail.com>
2025-08-23 18:51:19 -0400

    solved findword with leetcode explanatioon

--- index.js
+++ index.js
@@ -1,59 +1,84 @@
 class TrieNode {
   constructor() {
     this.children = new Map();
-    this.end = false;
+    this.isEnd = false;
   }
 }
 
-class WordDictionary {
+class Solution {
   constructor() {
     this.root = new TrieNode();
   }
 
   /**
-   * @param {string} word
-   * @return {void}
+   * @param {character[][]} board
+   * @param {string[]} words
+   * @return {string[]}
    */
-  addWord(word) {
+  findWords(board, words) {
+    for (let w of words) {
+      this.addWords(w);
+    }
+    let ROW = board.length;
+    let COL = board[0].length;
+
+    let res = new Set();
+    function dfs(i, j, root, word) {
+      if (i < 0 || i >= ROW || j < 0 || j >= COL || board[i][j] === "#") {
+        return;
+      }
+
+      let temp = board[i][j];
+      if (!root.children.has(temp)) return;
+
+      let nextNode = root.children.get(temp);
+      word = word + temp;
+
+      if (nextNode.isEnd) {
+        res.add(word);
+      }
+
+      board[i][j] = "#";
+
+      dfs(i + 1, j, nextNode, word);
+      dfs(i - 1, j, nextNode, word);
+      dfs(i, j + 1, nextNode, word);
+      dfs(i, j - 1, nextNode, word);
+
+      board[i][j] = temp;
+    }
+
+    for (let i = 0; i < ROW; i++) {
+      for (let j = 0; j < COL; j++) {
+        dfs(i, j, this.root, "");
+      }
+    }
+
+    return [...res];
+  }
+
+  addWords(word) {
     let cur = this.root;
 
-    for (let c of word) {
-      if (!cur.children.has(c)) {
-        cur.children.set(c, new TrieNode());
+    for (let w of word) {
+      if (!cur.children.has(w)) {
+        cur.children.set(w, new TrieNode());
       }
 
-      cur = cur.children.get(c);
+      cur = cur.children.get(w);
     }
 
-    cur.end = true;
-  }
-
-  /**
-   * @param {string} word
-   * @return {boolean}
-   */
-  search(word) {
-    function dfs(index, root) {
-      let cur = root;
-
-      for (let i = index; i < word.length; i++) {
-        if (word[i] == ".") {
-          for (let child of cur.children.values()) {
-            if (dfs(i + 1, child)) return true;
-          }
-
-          return false;
-        } else {
-          if (!cur.children.has(word[i])) {
-            return false;
-          } else {
-            cur = cur.children.get(word[i]);
-          }
-        }
-      }
-      return cur.end;
-    }
-
-    dfs(0, this.root);
+    cur.isEnd = true;
   }
 }
+
+let board = [
+  ["o", "a", "a", "n"],
+  ["e", "t", "a", "e"],
+  ["i", "h", "k", "r"],
+  ["i", "f", "l", "v"],
+];
+
+let words = ["oath", "pea", "eat", "rain"];
+let solver = new Solution();
+console.log(solver.findWords(board, words));// ["oath","eat"]
