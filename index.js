805604d55c63c20664110a3fab0433e059012ef8
ravigill3936 <gill1234ravi@gmail.com>
2025-08-26 03:58:48 -0400

    insert interval

--- index.js
+++ index.js
@@ -1,84 +1,44 @@
-class TrieNode {
-  constructor() {
-    this.children = new Map();
-    this.isEnd = false;
+/**
+ * @param {number[][]} intervals
+ * @param {number[]} newInterval
+ * @return {number[][]}
+ */
+function insert(intervals, newInterval) {
+  let res = [];
+
+  let i = 0;
+
+  while (i < intervals.length && intervals[i][1] < newInterval[0]) {
+    res.push(intervals[i]);
+    i++;
   }
+
+  while (i < intervals.length && intervals[i][0] <= newInterval[1]) {
+    newInterval[0] = Math.min(intervals[i][0], newInterval[0]);
+    newInterval[1] = Math.max(intervals[i][1], newInterval[1]);
+
+    i++;
+  }
+
+  res.push(newInterval);
+
+  while (i < intervals.length) {
+    res.push(intervals[i]);
+    i++;
+  }
+
+  return res;
 }
 
-class Solution {
-  constructor() {
-    this.root = new TrieNode();
-  }
-
-  /**
-   * @param {character[][]} board
-   * @param {string[]} words
-   * @return {string[]}
-   */
-  findWords(board, words) {
-    for (let w of words) {
-      this.addWords(w);
-    }
-    let ROW = board.length;
-    let COL = board[0].length;
-
-    let res = new Set();
-    function dfs(i, j, root, word) {
-      if (i < 0 || i >= ROW || j < 0 || j >= COL || board[i][j] === "#") {
-        return;
-      }
-
-      let temp = board[i][j];
-      if (!root.children.has(temp)) return;
-
-      let nextNode = root.children.get(temp);
-      word = word + temp;
-
-      if (nextNode.isEnd) {
-        res.add(word);
-      }
-
-      board[i][j] = "#";
-
-      dfs(i + 1, j, nextNode, word);
-      dfs(i - 1, j, nextNode, word);
-      dfs(i, j + 1, nextNode, word);
-      dfs(i, j - 1, nextNode, word);
-
-      board[i][j] = temp;
-    }
-
-    for (let i = 0; i < ROW; i++) {
-      for (let j = 0; j < COL; j++) {
-        dfs(i, j, this.root, "");
-      }
-    }
-
-    return [...res];
-  }
-
-  addWords(word) {
-    let cur = this.root;
-
-    for (let w of word) {
-      if (!cur.children.has(w)) {
-        cur.children.set(w, new TrieNode());
-      }
-
-      cur = cur.children.get(w);
-    }
-
-    cur.isEnd = true;
-  }
-}
-
-let board = [
-  ["o", "a", "a", "n"],
-  ["e", "t", "a", "e"],
-  ["i", "h", "k", "r"],
-  ["i", "f", "l", "v"],
+let intervals = [
+  [1, 2], // before newInterval
+  [3, 5], // overlaps newInterval
+  [6, 7], // overlaps newInterval
+  [8, 10], // after newInterval
 ];
 
-let words = ["oath", "pea", "eat", "rain"];
-let solver = new Solution();
-console.log(solver.findWords(board, words));// ["oath","eat"]
+let newInterval = [4, 6];
+
+let res = insert(intervals, newInterval);
+console.log(res);
+// Expected output: [[1, 2], [3, 7], [8, 10]]
