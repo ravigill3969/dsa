23275989c779cbdc15d647acd085079deffb9c13
ravigill3936 <gill1234ravi@gmail.com>
2025-08-27 18:37:56 -0400

    canAttendMeetings

--- index.js
+++ index.js
@@ -1,41 +1,38 @@
-class Solution {
-  /**
-   * @param {number[][]} intervals
-   * @return {number}
-   */
-  eraseOverlapIntervals(intervals) {
-    intervals.sort((a, b) => {
-      if (a[0] === b[0]) {
-        return a[1] - b[1];
-      }
-      return a[0] - b[0];
-    });
-
-    let res = 0;
-    let prevEnd = intervals[0][1];
-
-    for (let i = 1; i < intervals.length; i++) {
-      const [start, end] = intervals[i];
-      if (prevEnd <= start) {
-        prevEnd = end;
-      } else {
-        res++;
-        prevEnd = Math.min(end, prevEnd);
-      }
-    }
-
-    return res;
+class Interval {
+  constructor(start, end) {
+    this.start = start;
+    this.end = end;
   }
 }
 
+class Solution {
+  /**
+   * @param {Interval[]} intervals
+   * @returns {boolean}
+   */
+  canAttendMeetings(intervals) {
+    intervals.sort((a, b) => a.start - b.start);
+
+    let i = 0;
+
+    while (i < intervals.length - 1) {
+      if (intervals[i].end > intervals[i + 1].start) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+}
+
+let intervals = [
+  new Interval(0, 30),
+  new Interval(5, 10),
+  new Interval(15, 20),
+];
+
 let sol = new Solution();
 
-let res = sol.eraseOverlapIntervals(
-  (intervals = [
-    [1, 2],
-    [2, 4],
-    [1, 4],
-  ])
-);
+let res = sol.canAttendMeetings(intervals);
 
 console.log(res);
