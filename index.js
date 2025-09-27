5edaa1709ccf6373b59310c54ee777b82935f4c1
ravigill3936 <gill1234ravi@gmail.com>
2025-09-27 12:08:38 -0400

    subarray divisble by k

--- index.js
+++ index.js
@@ -1,33 +1,22 @@
-// Problem Statement
-
-// Given an integer array nums and an integer k, return the number of non-empty subarrays that have a sum divisible by k.
-
-// A subarray is a contiguous part of an array.
-
-/**
- * @param {number[]} nums
- * @param {number} k
- * @return {number}
- */
-var subarraysDivByK = function (nums, k) {
+function dbs(arr, k) {
   let sum = 0;
-  let count = 0;
+  let res = 0;
   let map = new Map();
-  map.set(0, 1);
 
-  for (let i = 0; i < nums.length; i++) {
-    sum += nums[i];
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
 
-    let mod = ((sum % k) + k) % k;
+    mod = ((sum % k) + k) % k;
 
     if (map.has(mod)) {
-      count += map.get(mod);
+      res += map.get(mod);
+      map.set(mod, map.get(mod) + 1);
+    } else {
+      map.set(mod, 1);
     }
-
-    map.set(mod, (map.get(mod) || 0) + 1);
   }
 
-  console.log(count);
-};
+  console.log(res);
+}
 
-subarraysDivByK((nums = [4, 5, 0, -2, -3, 1]), (k = 5));
+dbs([5, 0, 0, 1, -9, 3, 2], 5);
