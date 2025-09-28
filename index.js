6513c8fdacd63bb9b4c0c911e9d71c560bc698d0
ravigill3936 <gill1234ravi@gmail.com>
2025-09-28 14:31:05 -0400

    pair su

--- index.js
+++ index.js
@@ -1,26 +1,74 @@
+class ListNode {
+  constructor(val, next = null) {
+    this.val = val;
+    this.next = next;
+  }
+}
+
+function arrayToList(arr) {
+  if (arr.length === 0) return null;
+
+  let dummy = new ListNode(0);
+  let current = dummy;
+
+  for (let num of arr) {
+    current.next = new ListNode(num);
+    current = current.next;
+  }
+
+  return dummy.next; // head of the linked list
+}
+
+// Example
+let head = arrayToList([
+  47, 22, 81, 46, 94, 95, 90, 22, 55, 91, 6, 83, 49, 65, 10, 32, 41, 26, 83, 99,
+  14, 85, 42, 99, 89, 69, 30, 92, 32, 74, 9, 81, 5, 9,
+]);
 /**
- * @param {number[][]} triangle
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+
+/**
+ * @param {ListNode} head
  * @return {number}
  */
-var minimumTotal = function (triangle) {
-  let dp = [];
-  for (let i = 0; i < triangle.length; i++) {
-    dp.push(new Array(triangle[i].length).fill(undefined));
+var pairSum = function (head) {
+  if (!head) return null;
+
+  let slow = head;
+  let fast = head;
+
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
   }
 
-  let n = triangle.length;
-  function recursion(i, j) {
-    if (i === n - 1) return triangle[i][j];
+  let prev = null;
+  let cur = slow;
 
-    if (dp[i][j] !== undefined) return dp[i][j];
-
-    dp[i][j] =
-      triangle[i][j] + Math.min(recursion(i + 1, j), recursion(i + 1, j + 1));
-
-    return dp[i][j];
+  while (cur) {
+    let next = cur.next;
+    cur.next = prev;
+    prev = cur;
+    cur = next;
   }
 
-  return recursion(0, 0);
+  let max = 0;
+  let p1 = head,
+    p2 = prev;
+
+  while (p2) {
+    max = Math.max(p1.val + p2.val, max);
+    p2 = p2.next;
+    p1 = p1.next;
+  }
+
+  console.log(max);
+  return max
 };
 
-console.log(minimumTotal([[2], [3, 4], [6, 5, 7], [4, 1, 8, 3]]));
+pairSum(head);
