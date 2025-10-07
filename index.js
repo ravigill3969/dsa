6a71a44db842974c855ecd851e707fda3d3b95a6
ravigill3936 <gill1234ravi@gmail.com>
2025-10-07 16:41:14 -0400

    lowest comon ancestor

--- index.js
+++ index.js
@@ -1,33 +1,20 @@
 /**
- * @param {number[]} fruits
- * @return {number}
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
  */
-var totalFruit = function (fruits) {
-  let map = new Map();
-  let max = 0;
-
-  let i = 0;
-  let j = 0;
-
-  while (i < fruits.length) {
-    map.set(fruits[i], (map.get(fruits[i]) || 0) + 1);
-
-    while (map.size > 2) {
-      map.set(fruits[j], map.get(fruits[j]) - 1);
-
-      if (map.get(fruits[j]) <= 0) {
-        map.delete(fruits[j]);
-      }
-      j++;
-    }
-
-    max = Math.max(max, i - j + 1);
-
-    i++;
+var lowestCommonAncestor = function (root, p, q) {
+  if (root === null || root === p || root === q) {
+    return root;
   }
 
-  return max;
-};
+  const left = lowestCommonAncestor(root.left, p, q);
+  const right = lowestCommonAncestor(root.right, p, q);
 
-let res = totalFruit((fruits = [1, 2, 3, 2, 2]));
-console.log(res);
+  if (left && right) {
+    return root; // both sides found → this is LCA
+  }
+
+  return left ? left : right;
+};
