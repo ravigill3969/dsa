7b6ad5ae49ee7ced2c37e0c021fa2bb0593176f2
ravigill3936 <gill1234ravi@gmail.com>
2025-10-08 13:34:22 -0400

    successfull pairs

--- index.js
+++ index.js
@@ -1,56 +1,31 @@
-// Definition for singly-linked list.
-function ListNode(val, next) {
-  this.val = val === undefined ? 0 : val;
-  this.next = next === undefined ? null : next;
-}
+var successfulPairs = function (spells, potions, success) {
+  potions.sort((a, b) => a - b);
+  let res = [];
 
-function arrayToList(arr) {
-  if (arr.length === 0) return null;
+  let s = 0;
+  while (s < spells.length) {
+    let currentSpell = spells[s];
+    let l = 0;
+    let r = potions.length - 1;
+    let index = potions.length; // default: no valid potion
 
-  let head = new ListNode(arr[0]);
-  let current = head;
+    // binary search for first potion that succeeds
+    while (l <= r) {
+      let mid = Math.floor((l + r) / 2);
+      let product = currentSpell * potions[mid];
 
-  for (let i = 1; i < arr.length; i++) {
-    current.next = new ListNode(arr[i]);
-    current = current.next;
+      if (product >= success) {
+        index = mid;
+        r = mid - 1; // try to find an earlier one
+      } else {
+        l = mid + 1;
+      }
+    }
+
+    // all potions from 'index' to end are successful
+    res.push(potions.length - index);
+    s++;
   }
 
-  return head;
-}
-
-let l1 = arrayToList([1, 2, 3, 4]);
-let l2 = arrayToList([5, 6, 4]);
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-/**
- * @param {ListNode} head
- * @return {ListNode}
- */
-var swapPairs = function (head) {
-  if (!head || !head.next) return head;
-
-  let dummy = new ListNode(0, head);
-  let r = dummy;
-
-  while (r.next && r.next.next) {
-    let firstNode = r.next;
-    let secondNode = r.next.next;
-    let restOfList = secondNode.next;
-
-    r.next = secondNode;
-    secondNode.next = firstNode;
-    firstNode.next = restOfList;
-
-    r = firstNode;
-  }
-
-  console.log(JSON.stringify(dummy));
-  return dummy.next;
+  return res;
 };
-
-swapPairs(l1);
