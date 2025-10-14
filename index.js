81084e5d4c78292b0d49cc539d065aa1faec77dc
ravigill3936 <gill1234ravi@gmail.com>
2025-10-14 05:47:50 -0400

    tribonacci

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 /**
- * @param {number[]} nums
+ * @param {number} n
  * @return {number}
  */
-var rob = function (nums) {
-  let dp = new Array(nums.length).fill(0);
-  dp[0] = nums[0];
-  dp[1] = Math.max(nums[0], nums[1]);
+var tribonacci = function (n) {
+  let f = 0;
+  let s = 1;
+  let t = 1;
 
-  console.log(dp)
-
-  let res = 0;
-
-  for (let i = 2; i < nums.length; i++) {
-    dp[i] = Math.max(dp[i - 1], nums[i] + dp[i - 2]);
-    res = Math.max(dp[i], res);
+  n = n - 2;
+  while (n > 0) {
+    let sum = f + s + t;
+    f = s;
+    s = t;
+    t = sum;
+    n--;
   }
-  console.log(dp);
-  return res;
+
+  console.log(t);
 };
 
-rob((nums = [1, 3, 1]));
+let res = tribonacci((n = 4));
+console.log(res);
