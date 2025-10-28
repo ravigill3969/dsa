121279021ca754e7d439c5783a7549250f175794
ravigill3936 <gill1234ravi@gmail.com>
2025-10-28 15:48:34 -0400

    [200~maximalRectangle~

--- index.js
+++ index.js
@@ -1,35 +1,86 @@
 /**
- * @param {number[]} height
+ * @param {Int32Array[]} heights
  * @return {number}
  */
-var trap = function (height) {
-  let leftMax = new Array(height.length).fill(0);
-  let rightMax = new Array(height.length).fill(0);
+function largestHistogram(heights) {
+  let stack = [];
   let max = 0;
-  for (let i = 0; i < height.length; i++) {
-    leftMax[i] = max;
-    max = Math.max(max, height[i]);
+
+  for (let i = 0; i <= heights.length; i++) {
+    let h;
+    if (i === heights.length) {
+      h = 0; // sentinel
+    } else {
+      h = heights[i];
+    }
+
+    while (stack.length > 0 && h < heights[stack[stack.length - 1]]) {
+      let ele = heights[stack.pop()];
+      let area = 0;
+
+      if (stack.length === 0) {
+        area = i * ele;
+      } else {
+        area = ele * (i - stack[stack.length - 1] - 1);
+      }
+
+      if (area > max) {
+        max = area;
+      }
+    }
+
+    stack.push(i);
   }
 
-  max = 0;
-  for (let i = height.length - 1; i >= 0; i--) {
-    rightMax[i] = max;
-    max = Math.max(max, height[i]);
+  return max;
+}
+
+/**
+ * @param {character[][]} matrix
+ * @return {number}
+ */
+
+var maximalRectangle = function (matrix) {
+    if (matrix.length === 0) return 0;
+  let row = matrix.length;
+  let col = matrix[0].length;
+
+  let arr = [];
+
+  for (let i = 0; i < row; i++) {
+    let cArr = new Array(col).fill(0);
+    arr.push(cArr);
   }
 
-  let sum = 0;
+  for (let c = 0; c < col; c++) {
+    let sum = 0;
+    for (let r = 0; r < row; r++) {
+      if (matrix[r][c] === "0") {
+        sum = 0;
+      } else {
+        sum += 1;
+      }
 
-  for (let i = 0; i < height.length; i++) {
-    let min = Math.min(rightMax[i], leftMax[i]);
-    let res = min - height[i];
-
-    if (res > 0) {
-      sum += res;
+      arr[r][c] = sum;
     }
   }
 
-  return sum
+  let res = 0;
+  for (let r = 0; r < arr.length; r++) {
+    let histogram = arr[r];
+    let area = largestHistogram(histogram);
+
+    res = Math.max(area, res);
+  }
+
+  console.log(res);
 };
 
-let res = trap((height = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]));
-console.log(res);
+maximalRectangle(
+  (matrix = [
+    ["1", "0", "1", "0", "0"],
+    ["1", "0", "1", "1", "1"],
+    ["1", "1", "1", "1", "1"],
+    ["1", "0", "0", "1", "0"],
+  ])
+);
