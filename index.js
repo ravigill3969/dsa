55e90549795eb3c6859d1e7455213c0e77c7743d
ravigill3936 <gill1234ravi@gmail.com>
2025-11-02 15:28:30 -0500

    countUnguarded

--- index.js
+++ index.js
@@ -1,30 +1,73 @@
 /**
- * @param {string} s
+ * @param {number} m
+ * @param {number} n
+ * @param {number[][]} guards
+ * @param {number[][]} walls
  * @return {number}
  */
-var lengthOfLongestSubstring = function (s) {
-  let map = new Map();
+var countUnguarded = function (m, n, guards, walls) {
+  let arr = Array.from({ length: m }, () => new Array(n).fill(0));
   let res = 0;
-  let l = 0;
 
-  for (let i = 0; i < s.length; i++) {
-    while (map.has(s[i])) {
-      map.set(s[l], map.get(s[l]) - 1);
+  for (let [i, j] of guards) arr[i][j] = "G";
+  for (let [i, j] of walls) arr[i][j] = "W";
 
-      if (map.get(s[l]) == 0) {
-        map.delete(s[l]);
-      }
-      l++;
+  function goAllDirection(i, j) {
+    // down
+    for (let x = i + 1; x < m; x++) {
+      if (arr[x][j] === "W" || arr[x][j] === "G") break;
+      if (arr[x][j] === 0) arr[x][j] = -1;
     }
 
-    map.set(s[i], (map.get(s[i]) || 0) + 1);
+    // up
+    for (let x = i - 1; x >= 0; x--) {
+      if (arr[x][j] === "W" || arr[x][j] === "G") break;
+      if (arr[x][j] === 0) arr[x][j] = -1;
+    }
 
-    res = Math.max(i - l + 1, res);
+    // right
+    for (let y = j + 1; y < n; y++) {
+      if (arr[i][y] === "W" || arr[i][y] === "G") break;
+      if (arr[i][y] === 0) arr[i][y] = -1;
+    }
+
+    // left
+    for (let y = j - 1; y >= 0; y--) {
+      if (arr[i][y] === "W" || arr[i][y] === "G") break;
+      if (arr[i][y] === 0) arr[i][y] = -1;
+    }
   }
 
-  console.log(res)
-  return res
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (arr[i][j] === "G") {
+        goAllDirection(i, j);
+      }
+    }
+  }
+
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (arr[i][j] === 0) res++;
+    }
+  }
+
+  return res;
 };
 
-let res = lengthOfLongestSubstring("bbbbbbbb");
-console.log(res);
+let res = countUnguarded(
+  4,
+  6,
+  [
+    [0, 0],
+    [1, 1],
+    [2, 3],
+  ],
+  [
+    [0, 1],
+    [2, 2],
+    [1, 4],
+  ]
+);
+
+console.log(res); // ✅ correct output is 7
